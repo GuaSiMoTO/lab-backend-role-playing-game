f1bf56dc5bbee3f0836158e521e7dba678d35d41
Kilian <dankestmirror@hotmail.es>
2026-05-03 23:09:25 +0100

    unit pj basic

--- tests/unit/Combate.test.js
+++ tests/unit/Combate.test.js
@@ -1,1 +1,1 @@
-// Test 1 CalcularStats()
+// Test 1 Simular
--- tests/unit/Personaje.test.js
+++ tests/unit/Personaje.test.js
@@ -1,1 +1,28 @@
-//Test 2 simular()
+//Test 1 CalcularStats
+const Personaje = require('../../src/classes/Personaje')
+
+describe('Pruebas unitarias Personaje', () => {
+    test('calcularStats() Debería asignar los valores correctos para un humano guerrero', () => {
+
+        const datos = {
+            id: 1,
+            nombre: 'Aragorn',
+            especie: 'humano',
+            caregoria: 'guerrero'
+        };
+
+        const p = new Personaje(datos);
+
+        expect(p.vida).toBe(130);
+        experct(p.ataque).toBe(25);
+    });
+
+    test('Deberia lanzar un error con especie o categoria', () => {
+        const datosInvalidos = {
+            nombre: 'Invalido',
+            especie: 'orco',
+            categoria: 'guerrero'
+        };
+        expect(() => new Personaje(datosInvalidos)).toThrow();
+    })
+})
